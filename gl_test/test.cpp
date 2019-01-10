d23d95f3809c7e43b7907f75c3c083a27fd47912
Anson Chen <zhinan_chen@patac.com.cn>
2019-01-10 16:55:15 +0800

    off work, change the pc

--- gl_test/test.cpp
+++ gl_test/test.cpp
@@ -95,11 +95,28 @@ int main()
 	}
 
 	// set up vertex data(and buffer(s)) and configure vertex attributes
-		// ------------------------------------------------------------------
-	float vertices[] = {
+	// ------------------------------------------------------------------
+	
+	//vertices of triangle, for now they are not necessary
+	/*
+	float verticesOfTriangle[] = {
 		-0.5f, -0.5f, 0.0f, // left  
 		0.5f, -0.5f, 0.0f, // right 
 		0.0f,  0.5f, 0.0f  // top   
+	};
+	*/
+
+	//necessary vertices for a rectangle
+	float vertices[] = {
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+	//indicating how to use vertices to assemble multiple triangles
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
 	};
 
 	unsigned int VBO, VAO;
